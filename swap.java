fcf4ffee482254b1c5c28d9dda1a973baef9c012
Suhasygowda <164308589+Suhasygowda@users.noreply.github.com>
2025-06-19 15:00:40 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,17 @@
+// SwapNumbers.java
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        // Swapping with a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
